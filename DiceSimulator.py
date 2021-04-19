1c6a94753b8e5da0bbd821b4376d0cf0e0ec8199
Nikhil Yadav <nkrider27@gmail.com>
2021-04-19 19:25:29 +0530

    Update DiceSimulator.py
    Add more code to make this simulator more techy 
    Thanks!

--- DiceSimulator.py
+++ DiceSimulator.py
@@ -1,44 +1,60 @@
-import tkinter
+# INSTALL FOLLOWING PIP
+
+import tkinter                                 
 from PIL import Image, ImageTk
 import random
 
-# top-level widget which represents the main window of an application
+#  represents the main window of an application
+
 root = tkinter.Tk()
 root.geometry('400x400')
 root.title('DataFlair Roll the Dice')
 
 # Adding label into the frame
+
 BlankLine = tkinter.Label(root, text="")
 BlankLine.pack()
 
 # adding label with different font and formatting
-HeadingLabel = tkinter.Label(root, text="Hello from DataFlair!",
+
+HeadingLabel = tkinter.Label(root, text="Nikhil's Dice simulator",
    fg = "light green",
      bg = "dark green",
      font = "Helvetica 16 bold italic")
 HeadingLabel.pack()
 
-# images
+#  for images
+
 dice = ['die1.png', 'die2.png', 'die3.png',
     'die4.png', 'die5.png', 'die6.png']
-# simulating the dice with random numbers between
-# 0 to 6 and generating image
+
+# simulating the dice with random numbers between 0 to 6 and generating image
+
 DiceImage = ImageTk.PhotoImage(Image.open(random.choice(dice)))
 
 # construct a label widget for image
+
 ImageLabel = tkinter.Label(root, image=DiceImage)
 ImageLabel.image = DiceImage
 
 # packing a widget in the parent widget
+
 ImageLabel.pack( expand=True)
 
-# images
+#  for images
+
 dice = ['die1.png', 'die2.png', 'die3.png', 'die4.png', 'die5.png', 'die6.png']
 
 # simulating the dice with random numbers between
 # 0 to 6 and generating image
+
 DiceImage = ImageTk.PhotoImage(Image.open(random.choice(dice)))
 
-# call the mainloop of Tk
-# keeps window open
-root.mainloop()
+
+
+
+root.mainloop()                  # keeps window open
+
+
+
+
